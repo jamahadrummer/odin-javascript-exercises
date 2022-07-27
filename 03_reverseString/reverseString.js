6b0cff13c3d190ac6e75a72627bf6594c9945c0c
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-07-27 16:23:44 -0500

    reverseString pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('')
+    let reverseArray = array.reverse();
+    let joinArray = reverseArray.join('')
+    return joinArray
 };
 
 // Do not edit below this line
