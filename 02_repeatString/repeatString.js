b28afe7e2baf726e04d724747e91803597532c05
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-07-27 16:12:31 -0500

    repeatString Pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
 
-};
+const repeatString = function (s, num) {
+    let string = ''
+    if (num >= 0) {
+        for (let i = 1; i <= num; i++) {
+            string = string += s;
+        }
+        return string
+    } else {
+        return 'ERROR'
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
