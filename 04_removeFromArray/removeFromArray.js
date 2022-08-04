189b2d0238b88cc9c54b36f4b467f9cd0dde3fe3
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-04 10:02:58 -0500

    remove from array test 1 & 2 pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,21 @@
 const removeFromArray = function () {
-    let arr = arguments[0];
-    let arg1 = arr.indexOf(arguments[1])
-    let arg2 = arr.indexOf(arguments[2])
-    arr.splice(arg1, 1)
-    arr.splice(arg2, 1)
-    return arr;
+    if (arguments[2] === undefined) {
+        let arr = arguments[0];
+        if (arr.indexOf(arguments[1]) != '-1');
+        let arg1 = arr.indexOf(arguments[1]);
+        arr.splice(arg1, 1);
+        return arr;
+    } else {
+        let arr = arguments[0];
+        if (arr.indexOf(arguments[1]) != '-1'
+            && arguments[2] != '-1') {
+            let arg1 = arr.indexOf(arguments[1]);
+            let arg2 = arr.indexOf(arguments[2]);
+            arr.splice(arg1, 1);
+            arr.splice(arg2, 1);
+            return arr;
+        };
+    };
 };
 
 // Do not edit below this line
