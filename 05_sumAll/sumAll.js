4fe2118e5b88bf1a4522d6902e52158b8bb05c91
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-04 13:20:30 -0500

    sumall first 1-3 pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function () {
+    let arr = []
+    if (arguments[0] < arguments[1]) {
+        for (let i = arguments[0]; i <= arguments[1]; i++) {
+            arr.push(i)
+        }
+    } else if (arguments[0] > arguments[1]) {
+        for (let i = arguments[0]; i >= arguments[1]; i--) {
+            arr.push(i)
+        }
+    } else if (arguments[0] < 0 || argument[1] < 0) {
+        console.log('ERROR')
+    }
+    console.log(arr)
+    let finalSum = arr.reduce((a, b) => a + b, 0)
+    return finalSum
 };
 
 // Do not edit below this line
