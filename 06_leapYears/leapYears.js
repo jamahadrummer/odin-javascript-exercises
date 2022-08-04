11b8dac3f0aa56224bdffc1c4f681626b2c317a8
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-04 14:59:09 -0500

    leapYears all pass

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function () {
+    if ((arguments[0] % 4) == 0) {
+        if ((arguments[0] % 100) == 0) {
+            if ((arguments[0] % 400) == 0) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return true
+        }
+    } else {
+        return false
+    }
 
 };
 
