ed4e8566b734d23ef9edd5ea25a42af7dd663f62
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-04 14:19:41 -0500

    sumall first all pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,23 @@
 const sumAll = function () {
     let arr = []
-    if (arguments[0] < arguments[1]) {
-        for (let i = arguments[0]; i <= arguments[1]; i++) {
-            arr.push(i)
+    if (typeof arguments[0] === 'number' && typeof arguments[1] === 'number') {
+        if (arguments[0] < 0 || arguments[1] < 0) {
+            return 'ERROR'
+        } else if (arguments[0] > arguments[1]) {
+            for (let i = arguments[0]; i >= arguments[1]; i--) {
+                arr.push(i)
+            }
+        } else if (arguments[0] < arguments[1]) {
+            for (let i = arguments[0]; i <= arguments[1]; i++) {
+                arr.push(i)
+            }
         }
-    } else if (arguments[0] > arguments[1]) {
-        for (let i = arguments[0]; i >= arguments[1]; i--) {
-            arr.push(i)
-        }
-    } else if (arguments[0] < 0 || argument[1] < 0) {
-        console.log('ERROR')
+        console.log(arr)
+        let finalSum = arr.reduce((a, b) => a + b, 0)
+        return finalSum
+    } else {
+        return 'ERROR'
     }
-    console.log(arr)
-    let finalSum = arr.reduce((a, b) => a + b, 0)
-    return finalSum
 };
 
 // Do not edit below this line
