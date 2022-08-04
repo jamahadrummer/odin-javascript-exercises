45b64b97e1afbd00da3791ce56085c0f174fb623
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-04 11:51:19 -0500

    remove from array all pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,23 @@
 const removeFromArray = function () {
+    let arr = arguments[0]
     if (arguments[2] === undefined) {
         let arr = arguments[0];
         if (arr.indexOf(arguments[1]) != '-1');
         let arg1 = arr.indexOf(arguments[1]);
         arr.splice(arg1, 1);
         return arr;
+    } else if (arr.indexOf(arguments[2]) != '-1') {
+        for (let i = 1; i < arguments.length; i++)
+            if (arr.indexOf(arguments[i] != '-1')) {
+                let arg = arr.indexOf(arguments[i]);
+                if (arg != '-1') {
+                    arr.splice(arg, 1);
+                }
+            } else {
+                return arr;
+            } return arr;
     } else {
-        let arr = arguments[0];
-        if (arr.indexOf(arguments[1]) != '-1'
-            && arguments[2] != '-1') {
-            let arg1 = arr.indexOf(arguments[1]);
-            let arg2 = arr.indexOf(arguments[2]);
-            arr.splice(arg1, 1);
-            arr.splice(arg2, 1);
-            return arr;
-        };
+        return arr;
     };
 };
 
