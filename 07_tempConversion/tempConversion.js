c77ae595d9e38f907ab75f7f17115d62aac37bfe
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-04 15:29:07 -0500

    tempConversion in progress

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function (fIn) {
+  let cOut = (32 - fIn) * 5 / 9
+  if (Number.isInteger(cOut)) {
+    return cOut
+  } else {
+    let cRound = cOut.toFixed(1)
+    return cRound
+  }
 };
 
-const ctof = function() {
-
+const ctof = function (cIn) {
+  let fOut = (cIn * 9 / 5 + 32)
+  if (Number.isInteger(fOut)) {
+    return fOut
+  } else {
+    let fRound = cOut.toFixed(1)
+    return fRound
+  }
 };
 
 // Do not edit below this line
