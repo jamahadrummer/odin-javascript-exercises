bd5d53487afc39419d54314bf0c3385f02d01bdb
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-04 09:27:03 -0500

    remove from array test 2 pass, broke test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function (arr, rem) {
-    arr.splice(arr.indexOf(rem), 1);
-    return arr
+const removeFromArray = function () {
+    let arr = arguments[0];
+    let arg1 = arr.indexOf(arguments[1])
+    let arg2 = arr.indexOf(arguments[2])
+    arr.splice(arg1, 1)
+    arr.splice(arg2, 1)
+    return arr;
 };
 
 // Do not edit below this line
