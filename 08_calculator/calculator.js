886a6c2071c862f3d53325e6e4a0518a11d4fb89
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-09 15:40:46 -0500

    calculator progress

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (arg1, arg2) {
+  let sol = (arg1 + arg2)
+  return sol
 };
 
-const subtract = function() {
-	
+const subtract = function (arg1, arg2) {
+  let sol = (arg1 - arg2)
+  return sol
 };
 
-const sum = function() {
-	
+const sum = function (arg) {
+  let arr = arg
+  for (let i = 0; i <= arr.length; i++) {
+    let finalSum = arr.reduce((a, b) => a + b, 0)
+    return finalSum
+  }
 };
 
-const multiply = function() {
+const multiply = function (arg1, arg2) {
+  let sol = (arg1 * arg2)
+  return sol
+};
+
+const power = function () {
 
 };
 
-const power = function() {
-	
-};
+const factorial = function () {
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
