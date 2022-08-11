9e66b2f0af171eb6e39246201b389c8023e43d62
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-11 12:17:34 -0500

    palindrome all pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringLower = string.toLowerCase()
+    let stringBasic = stringLower.replace(/[^a-z]/g, '')
+    let revString = function () {
+        let stringArr = []
+        for (let i = stringBasic.length - 1; i >= 0; i--) {
+            stringArr.push(stringBasic[i]);
+        }
+        return stringArr.join('')
+    }
+    if (revString(stringBasic) === stringBasic) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
