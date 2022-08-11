91c368344ddce152032038ce4dcd75d438b19165
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-11 15:41:09 -0500

    findTheOldest passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arg) {
+    const date = new Date();
+    let year = date.getFullYear();
+    let tempArr = arg;
+    let oldestAge = 0;
+    let oldestPerson = 0;
+    arg.forEach(person => {
+        if (typeof person.yearOfDeath != 'undefined') {
+            let age = person.yearOfDeath - person.yearOfBirth
+            person.age = age
+        } else {
+            let age = year - person.yearOfBirth
+            person.age = age
+        }
+    })
+    tempArr.forEach(person => {
+        if (person.age > oldestAge) {
+            oldestAge = person.age
+        }
+    })
+    tempArr.forEach(person => {
+        if (oldestAge === person.age)
+            oldestPerson = person
+    })
+    return oldestPerson
 };
 
 // Do not edit below this line
