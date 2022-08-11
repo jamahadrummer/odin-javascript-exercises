7a4eca179a09ff5b6d0b8ea920e98fab1b034355
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-11 13:02:41 -0500

    fibonacci all pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (arg) {
+    if (arg < 0) {
+        return 'OOPS'
+    } else {
+        let fibArr = []
+        fibArr[0] = 0;
+        fibArr[1] = 1
+        for (let i = 2; i <= arg; i++) {
+            fibArr[i] = fibArr[i - 1] + fibArr[i - 2]
+        }
+        return fibArr[arg]
+    };
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
