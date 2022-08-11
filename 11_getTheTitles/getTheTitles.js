0a5e1816ae3df3784f25e18d95bd225c1d5a6679
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2022-08-11 13:39:12 -0500

    getTheTitles passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function (arg) {
+    let arr = []
+    arg.forEach(object => {
+        arr.push(object.title)
+    })
+    return arr
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
